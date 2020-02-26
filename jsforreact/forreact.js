5c27110b87fa7dcc5fd506ed682385565e07cf05
satyavarma <rudrarajusatyavarma@gmail.com>
2020-02-27 02:40:08 +0530

    Trying to solve err

---
+++ jsforreact/forreact.js
@@ -0,0 +1,18 @@
+function dispr(nn){
+    for(var i;i<nn;i++){
+        console.log(i)
+    }
+    console.log(i)
+}
+function disprlet(nn){
+    for(let i=0;i<nn;i++){
+        console.log(i);
+    }
+    console.log(i);
+}
+dispr(10)
+disprlet(10)
+const c = 10;
+console.log(c)
+c =20
+console.log(ff)
